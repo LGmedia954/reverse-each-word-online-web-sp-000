32331d6126cd7e5d4e0b6a5a3e8651e1e807fb60
Leslie G <lesliegonzalez@me.com>
2020-07-03 22:26:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  (sentence.split).each do |words|
+  sentence.reverse.split).each do |words|
   puts words.reverse
   end
   sentence
